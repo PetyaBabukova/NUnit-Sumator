ad1af689d59030bae41b1471eb0155fdc2431560
Petya Babukova <p_babukova@abv.bg>
2023-02-14 15:55:15 +0200

    Add project files.

---
+++ Sumator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumata e ravna na: " + Sumator.Sum(new int[] { 1, 2, 3 }));
+        }
+    }
+}
---
+++ Sumator/Sumator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumator
+{
+    public static class Sumator
+    {
+        public static int Sum(int[] arr) 
+        {
+               int sum = arr[0];
+
+        for(int i = 1; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
