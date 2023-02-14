33ee1547aee629d56a0339e13d4b99ab2fc91d6e
Petya Babukova <p_babukova@abv.bg>
2023-02-14 16:38:21 +0200

    Initialise test project

---
+++ Sumator.UnuitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sumator.UnuitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
